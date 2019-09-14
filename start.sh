fee2bae5145566494060bcb5a1a3d09838148d59
John Brockman <johndoubleub@gmail.com>
2019-09-14 19:29:06 +0100

    Reverted without shfiles

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -jar App.jar & echo $! > ./pid.file &
-#Starts the app and saves the process id in a file
--- stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kill $(cat ./pid.file)
-#Kills the app using the saved process id
