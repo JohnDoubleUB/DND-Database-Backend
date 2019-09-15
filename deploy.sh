20c9f7aa3dad835fde3bf1cd71f55c9c7c41bc36
John Brockman <johndoubleub@gmail.com>
2019-09-15 08:26:43 +0100

    More docker tests

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-project=${PWD##*/}
+#project=${PWD##*/}
 host_port=9000
 container_port=9000
-mvn clean package
-docker stop ${project}
-docker rm ${project}
-docker build -t dnddb .
-docker run -d -p ${host_port}:${container_port} dnddb
+
+imageName=dnddb:latest
+containerName=dnddb-container
+
+docker build -t $imageName -f Dockerfile  .
+
+echo Delete old container...
+docker rm -f $containerName
+
+echo Run new container...
+docker run -d -p ${host_port}:${container_port} --name $containerName $imageName
+
+
+
+
+#docker stop ${project}
+#docker rm ${project}
+#docker build -t dnddb .
+#docker run -d -p ${host_port}:${container_port} dnddb
 
 ##I don't know how much of this will work but I did manage to get docker to work so thats a start!
