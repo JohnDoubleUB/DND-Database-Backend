1133f42bbc53cedb9b11049020f361cf6a055d62
John Brockman <johndoubleub@gmail.com>
2019-09-15 18:18:48 +0100

    Added Selenium Dependency, added chrome driver, and created Selenium test class

---
+++ src/test/java/com/qa/SeleniumTests.java
@@ -0,0 +1,20 @@
+package com.qa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTests {
+    private ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+}
