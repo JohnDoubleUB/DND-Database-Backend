19b1fb50065371496ce0468438d79d67780cfc26
John Brockman <johndoubleub@gmail.com>
2019-09-15 18:30:43 +0100

    prepared setup(@Before) and teardown(@After) for selenium testing

--- src/test/java/com/qa/SeleniumTests.java
+++ src/test/java/com/qa/SeleniumTests.java
@@ -2,6 +2,7 @@ package com.qa;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTests {
@@ -9,12 +10,20 @@ public class SeleniumTests {
 
     @Before
     public void setUp(){
-
+        //Establish chrome driver location
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @After
     public void tearDown(){
+        driver.close();
+    }
 
+    @Test
+    public void navigationTest() {
+        driver.get();
     }
 
 }
