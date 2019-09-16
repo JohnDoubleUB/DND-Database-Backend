09e0549008909eb87fb8ceb314130113cead78cb
John Brockman <johndoubleub@gmail.com>
2019-09-16 19:44:28 +0100

    More experimentation changes made to docker file3

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,6 @@ containerName=dnddb-container
 
 docker build -t $imageName -f Dockerfile  .
 
-## Logic to ensure docker is correctly handled!
-if [ ! "$(docker ps -q -f name=$containerName)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=$containerName)" ]; then
-        # cleanup
-        echo Delete old container...
-        docker rm -f $containerName
-    fi
-    # run your container
-    echo Run new container...
-    docker run -d -p ${host_port}:${container_port} --name $containerName $imageName
-fi
+# run your container
+echo Run new container...
+docker run -d -p ${host_port}:${container_port} --name $containerName $imageName
