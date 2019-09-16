5bd4bdc8693c6261ea18d091931296fa9a9df9b1
John Brockman <johndoubleub@gmail.com>
2019-09-16 19:41:52 +0100

    More experimentation changes made to docker file2

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#project=${PWD##*/}
 host_port=9000
 container_port=9000
 
@@ -19,13 +18,3 @@ if [ ! "$(docker ps -q -f name=$containerName)" ]; then
     echo Run new container...
     docker run -d -p ${host_port}:${container_port} --name $containerName $imageName
 fi
-
-
-
-
-#docker stop ${project}
-#docker rm ${project}
-#docker build -t dnddb .
-#docker run -d -p ${host_port}:${container_port} dnddb
-
-##I don't know how much of this will work but I did manage to get docker to work so thats a start!
