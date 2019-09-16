516b06d366cc4eb30c93361658706ad48527dfc0
John Brockman <johndoubleub@gmail.com>
2019-09-16 20:20:42 +0100

    Made tweaks to close.sh

--- close.sh
+++ close.sh
@@ -3,8 +3,8 @@
 containerName=dnddb-container
 
 # Check if docker is already running. kill if it is!
-if [ ! "$(docker ps -q -f name=$containerName)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=$containerName)" ]; then
+if [ "$(docker ps -q -f name=$containerName)" ]; then
+    if [ ! "$(docker ps -aq -f status=exited -f name=$containerName)" ]; then
         # cleanup
         echo Delete old container...
         docker rm -f $containerName
