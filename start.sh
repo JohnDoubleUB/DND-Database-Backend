dbb45fdf2eb3165e9035cf07a318be13a13baa04
John Brockman <johndoubleub@gmail.com>
2019-09-16 20:42:18 +0100

    Made tweaks to start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,23 @@ imageName=dnddb:latest
 containerName=dnddb-container
 
 # Check if the image exists, only build if it does!
-if [[ "$(docker images -q $imageName 2> /dev/null)" != "" ]]; then
+
+if [ "$(docker images -q $imageName)" ]; then
   echo Run new container...
   docker run -d -p ${host_port}:${container_port} --name $containerName $imageName
 fi
+
+
+#
+#if [ "$(docker ps -q -f name=$containerName)" ]; then
+#    if [ ! "$(docker ps -aq -f status=exited -f name=$containerName)" ]; then
+#        # cleanup
+#        echo Delete old container...
+#        docker rm -f $containerName
+#    fi
+#fi
+#
+#if [[ "$(docker images -q $imageName 2> /dev/null)" != "" ]]; then
+#  echo Run new container...
+#  docker run -d -p ${host_port}:${container_port} --name $containerName $imageName
+#fi
