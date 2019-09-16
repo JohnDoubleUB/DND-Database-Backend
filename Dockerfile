d5749d7a4cf2eaf287c252ca1c54717d6c7de2ff
John Brockman <johndoubleub@gmail.com>
2019-09-16 20:08:01 +0100

    More experimentation changes made to docker file4

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM maven as build
 WORKDIR /build
 COPY . .
-RUN mvn package
+RUN mvn package -Dmaven.test.skip=true
 CMD mvn spring-boot:run
 EXPOSE 9000
---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+host_port=9000
+container_port=9000
+
+imageName=dnddb:latest
+containerName=dnddb-container
+
+# Check if the image exists, only build if it does!
+if [[ "$(docker images -q $imageName 2> /dev/null)" == "" ]]; then
+  echo Run new container...
+  docker run -d -p ${host_port}:${container_port} --name $containerName $imageName
+fi
