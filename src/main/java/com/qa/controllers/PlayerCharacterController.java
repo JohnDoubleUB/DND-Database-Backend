c3605f525155dbec71a876436176f08b0cfb0470
John Brockman <johndoubleub@gmail.com>
2019-09-19 20:37:37 +0100

    Finished creation of PlayerCharacterService Class

--- src/main/java/com/qa/controllers/PlayerCharacterController.java
+++ src/main/java/com/qa/controllers/PlayerCharacterController.java
@@ -15,26 +15,26 @@ public class PlayerCharacterController {
     @Autowired
     private PlayerCharacterRepository repository;
 
-    //Get all characters
+    //Get all characters - DONE THIS ONE
     @RequestMapping(value = "characters", method = RequestMethod.GET)
     public List<PlayerCharacter> getCharacters(){
         return repository.findAll();
     }
 
-    //Add a new character
+    //Add a new character - DONE THIS ONE
     @RequestMapping(value = "characters", method = RequestMethod.POST)
     public PlayerCharacter addCharacter(@RequestBody PlayerCharacter player){
         return repository.saveAndFlush(player);
     }
 
-    //Get a specific character
+    //Get a specific character - DONE THIS ONE
     @RequestMapping(value = "characters/{id}", method = RequestMethod.GET)
     public PlayerCharacter getCharacter(@PathVariable Long id){
-        return repository.findById(id);
-        return repository.findOne(id);
+//        return repository.findById(id);
+//        return repository.findOne(id);
     }
 
-    //Delete a specific character
+    //Delete a specific character -- DONE THIS ONE
     @RequestMapping(value = "characters/{id}", method = RequestMethod.DELETE)
     public PlayerCharacter deleteCharacter(@PathVariable Long id){
         PlayerCharacter existing = repository.findOne(id);
@@ -42,7 +42,7 @@ public class PlayerCharacterController {
         return existing;
     }
 
-    //Update Character by id
+    //Update Character by id -- DONE!
     @Transactional
     @RequestMapping(value = "characters/{id}", method = RequestMethod.PUT)
     public PlayerCharacter updateCharacter(@PathVariable Long id, @RequestBody PlayerCharacter player){
--- src/main/java/com/qa/service/PlayerCharacterService.java
+++ src/main/java/com/qa/service/PlayerCharacterService.java
@@ -11,36 +11,74 @@ import java.util.List;
 
 @Service
 public class PlayerCharacterService {
+
     @Autowired
     private PlayerCharacterRepository repository;
 
 
-    public List<PlayerCharacterDto> getPlayerCharacters(){
+    public List<PlayerCharacterDto> getCharacters(){
         List<PlayerCharacter> playerCharacters = repository.findAll();
         List<PlayerCharacterDto> playerCharactersDto = new ArrayList<PlayerCharacterDto>();
         playerCharacters.forEach(pc -> playerCharactersDto.add(new PlayerCharacterDto(pc)));
         return playerCharactersDto;
     }
 
-    public PlayerCharacterDto addCharacter(PlayerCharacterDto playerCharactersDto){
-        playerCharactersDto.setId(null);
+    public PlayerCharacterDto addCharacter(PlayerCharacterDto playerCharacterDto){
+        playerCharacterDto.setId(null);
         PlayerCharacter playerCharacter = PlayerCharacter.createPlayerCharacter();
 
-        playerCharacter.setName(playerCharactersDto.getName());
-        playerCharacter.setRace(playerCharactersDto.getRace());
-        playerCharacter.setPlayerClass(playerCharactersDto.getPlayerClass());
-        playerCharacter.setAlignment(playerCharactersDto.getAlignment());
-        playerCharacter.setBackground(playerCharactersDto.getBackground());
-        playerCharacter.setLevel(playerCharactersDto.getLevel());
-        playerCharacter.setBaseStr(playerCharactersDto.getBaseStr());
-        playerCharacter.setBaseInt(playerCharactersDto.getBaseInt());
-        playerCharacter.setBaseDex(playerCharactersDto.getBaseDex());
-        playerCharacter.setBaseCon(playerCharactersDto.getBaseCon());
-        playerCharacter.setBaseWis(playerCharactersDto.getBaseWis());
-        playerCharacter.setBaseCha(playerCharactersDto.getBaseCha());
-        playerCharacter.setBaseHP(playerCharactersDto.getBaseHP());
-        playerCharacter.setBaseProficiency(playerCharactersDto.getBaseProficiency());
-        return new PlayerCharacterDto()
+        playerCharacter.setName(playerCharacterDto.getName());
+        playerCharacter.setRace(playerCharacterDto.getRace());
+        playerCharacter.setPlayerClass(playerCharacterDto.getPlayerClass());
+        playerCharacter.setAlignment(playerCharacterDto.getAlignment());
+        playerCharacter.setBackground(playerCharacterDto.getBackground());
+        playerCharacter.setLevel(playerCharacterDto.getLevel());
+        playerCharacter.setBaseStr(playerCharacterDto.getBaseStr());
+        playerCharacter.setBaseInt(playerCharacterDto.getBaseInt());
+        playerCharacter.setBaseDex(playerCharacterDto.getBaseDex());
+        playerCharacter.setBaseCon(playerCharacterDto.getBaseCon());
+        playerCharacter.setBaseWis(playerCharacterDto.getBaseWis());
+        playerCharacter.setBaseCha(playerCharacterDto.getBaseCha());
+        playerCharacter.setBaseHP(playerCharacterDto.getBaseHP());
+        playerCharacter.setBaseProficiency(playerCharacterDto.getBaseProficiency());
+
+        return new PlayerCharacterDto(repository.saveAndFlush(playerCharacter));
     }
+
+    public PlayerCharacterDto getCharacter(Long id){
+        PlayerCharacter playerCharacter = repository.getOne(id);
+        return new PlayerCharacterDto(playerCharacter);
+    }
+
+    public PlayerCharacterDto deleteCharacter(Long id){
+        PlayerCharacter playerCharacter = repository.getOne(id);
+        PlayerCharacterDto playerCharacterDto = new PlayerCharacterDto(playerCharacter);
+        repository.deleteById(id);
+        return playerCharacterDto;
+    }
+
+    public PlayerCharacterDto updateCharacter(Long id, PlayerCharacterDto playerCharacterDto){
+        PlayerCharacter playerCharacter = repository.getOne(id);
+
+        playerCharacter.setName(playerCharacterDto.getName());
+        playerCharacter.setRace(playerCharacterDto.getRace());
+        playerCharacter.setPlayerClass(playerCharacterDto.getPlayerClass());
+        playerCharacter.setAlignment(playerCharacterDto.getAlignment());
+        playerCharacter.setBackground(playerCharacterDto.getBackground());
+        playerCharacter.setLevel(playerCharacterDto.getLevel());
+        playerCharacter.setBaseStr(playerCharacterDto.getBaseStr());
+        playerCharacter.setBaseInt(playerCharacterDto.getBaseInt());
+        playerCharacter.setBaseDex(playerCharacterDto.getBaseDex());
+        playerCharacter.setBaseCon(playerCharacterDto.getBaseCon());
+        playerCharacter.setBaseWis(playerCharacterDto.getBaseWis());
+        playerCharacter.setBaseCha(playerCharacterDto.getBaseCha());
+        playerCharacter.setBaseHP(playerCharacterDto.getBaseHP());
+        playerCharacter.setBaseProficiency(playerCharacterDto.getBaseProficiency());
+
+        repository.flush();
+        return new PlayerCharacterDto(playerCharacter);
+    }
+
+
 
 }
