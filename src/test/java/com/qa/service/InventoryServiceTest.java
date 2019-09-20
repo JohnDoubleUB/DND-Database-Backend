bb0effd0a5c01cb8b06c9c575f578747c4c1d6f0
johndoubleub <up741132@myport.ac.uk>
2019-09-20 10:00:37 +0100

    Created InventoryServiceTest template and PlayerCharacterServiceTest template

---
+++ src/test/java/com/qa/service/InventoryServiceTest.java
@@ -0,0 +1,58 @@
+package com.qa.service;
+
+import com.qa.persistence.repository.InventoryRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InventoryServiceTest {
+
+    @Mock
+    private InventoryRepository repository;
+
+    @InjectMocks
+    private InventoryService inventoryService;
+
+    @Test
+    public void getInventoriesTest(){
+
+    }
+
+    @Test
+    public void addInventoryTest(){
+
+    }
+
+    @Test
+    public void getInventoryTest(){
+
+    }
+
+    @Test
+    public void getInventoryByPlayerIdTest(){
+
+    }
+
+    @Test
+    public void deleteInventoryTest(){
+
+    }
+
+    @Test
+    public void deleteInventoryByPlayerIdTest(){
+
+    }
+
+    @Test
+    public void updateInventoryTest(){
+
+    }
+
+    @Test
+    public void updateInventoryByPlayerIdTest(){
+
+    }
+}
---
+++ src/test/java/com/qa/service/PlayerCharacterServiceTest.java
@@ -0,0 +1,44 @@
+package com.qa.service;
+
+import com.qa.persistence.repository.PlayerCharacterRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PlayerCharacterServiceTest {
+
+    @Mock
+    private PlayerCharacterRepository repository;
+
+    @InjectMocks
+    private PlayerCharacterService playerCharacterService;
+
+    @Test
+    public void getCharactersTest(){
+
+    }
+
+    @Test
+    public void getCharacterTest(){
+
+    }
+
+    @Test
+    public void addCharacterTest(){
+
+    }
+
+    @Test
+    public void deleteCharacterTest(){
+
+    }
+
+    @Test
+    public void updateCharacterTest(){
+
+    }
+
+}
