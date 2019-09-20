9761205e878429e51296e333f25ab73cdf133249
johndoubleub <up741132@myport.ac.uk>
2019-09-20 15:30:00 +0100

    Added One PlayerCharacterController Test

--- src/test/java/com/qa/service/PlayerCharacterServiceTest.java
+++ src/test/java/com/qa/service/PlayerCharacterServiceTest.java
@@ -1,11 +1,19 @@
 package com.qa.service;
 
+import com.qa.dto.PlayerCharacterDto;
+import com.qa.persistence.models.PlayerCharacter;
 import com.qa.persistence.repository.PlayerCharacterRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PlayerCharacterServiceTest {
@@ -18,7 +26,21 @@ public class PlayerCharacterServiceTest {
 
     @Test
     public void getCharactersTest(){
+        List<PlayerCharacter> playerCharacters = new ArrayList<>();
+        List<PlayerCharacterDto> playerCharacterDtos = new ArrayList<PlayerCharacterDto>();
 
+        playerCharacters.add(new PlayerCharacter(2L, "Socrowtes", "Kenku", "Monk", "Chaotic Neutral", "Criminal", 4, 10, 10, 15, 17, 10, 28, 22, 2 ));
+        playerCharacters.add(new PlayerCharacter(1L, "CrowBar", "Kenku", "Bard", "Neutral", "Noble", 4, 10, 14, 15, 12, 10, 30, 22, 4 ));
+
+        playerCharacterDtos.add(new PlayerCharacterDto(playerCharacters.get(0)));
+        playerCharacterDtos.add(new PlayerCharacterDto(playerCharacters.get(1)));
+
+        Mockito.when(repository.findAll()).thenReturn(playerCharacters);
+
+        assertEquals("Socrowtes", playerCharacterService.getCharacters().get(0).getName());
+        assertEquals(new Long(2L), playerCharacterService.getCharacters().get(0).getId());
+        assertEquals("CrowBar", playerCharacterService.getCharacters().get(1).getName());
+        assertEquals(new Long(1L), playerCharacterService.getCharacters().get(1).getId());
     }
 
     @Test
