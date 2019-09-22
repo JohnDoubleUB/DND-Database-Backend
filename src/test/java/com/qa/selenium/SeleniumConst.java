7006d834a4db348f6b745ca2a5f9f0ea30391dee
John Brockman <johndoubleub@gmail.com>
2019-09-22 07:17:28 +0100

    Made minor changes to selenium constants

--- src/test/java/com/qa/selenium/SeleniumConst.java
+++ src/test/java/com/qa/selenium/SeleniumConst.java
@@ -3,9 +3,11 @@ package com.qa.selenium;
 public class SeleniumConst {
     public static final String DRIVER_KEY= "webdriver.chrome.driver";
 
-    public static final String DRIVER_LOCATION = "src/test/resources/chromedriver.exe";
+    public static final String DRIVER_LOCATION = "src/test/resources/chromedriver";
 
-    //public static final String HOMEPAGE_URL = "http://localhost/";
+    //public static final String DRIVER_LOCATION = "src/test/resources/chromedriver.exe";
 
-    public static final String HOMEPAGE_URL = "file:///C:/Users/johnw/Desktop/QAIndividualProj/QASoloProject/FrontEnd/";
+    public static final String HOMEPAGE_URL = "http://localhost/";
+
+    //public static final String HOMEPAGE_URL = "file:///C:/Users/johnw/Desktop/QAIndividualProj/QASoloProject/FrontEnd/";
 }
